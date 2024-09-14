4d54a67b2f162e13aa5ffd9175d0e63b313c42cc
Ibragim <97885427+Heilonng23@users.noreply.github.com>
2024-09-14 11:56:29 +0000

    Sep 14, 2024, 3:56 PM

--- src/useMovies.jsx
+++ src/useMovies.jsx
@@ -1,1 +1,43 @@
-export function useMovies() {}
+export function useMovies() {
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchedMovies() {
+      try {
+        setIsLoading(true);
+        setError("");
+        const res = await fetch(
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal }
+        );
+
+        if (!res.ok) throw new Error("Something went wrong");
+
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("Movie not found");
+
+        setMovies(data.Search);
+        setError("");
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.log(err);
+          setError(err.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (query.length < 3) {
+      setMovies([]);
+      setError("");
+      return;
+    }
+
+    handleCloseMovie();
+    fetchedMovies();
+
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+}
